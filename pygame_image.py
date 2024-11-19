e738f6aba6c7626739ea35862e1e95006142006d
Ayato <c0a2314523@edu.teu.ac.jp>
2024-11-19 17:06:29 +0900

    課題:なにもしないと飛ばされるこうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -17,28 +17,39 @@ def main():
     bird_rct.center = 300, 200
     tmr = 0
     while True:
+        
         key_lst = pg.key.get_pressed()
+
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            
+            if event.type == pg.QUIT: 
+                return
         x = -(tmr%3200)
         screen.blit(bg_img, [(x), 0])
         screen.blit(bg_img2, [(x+1600), 0])
         screen.blit(bg_img, [(x+3200), 0])
         screen.blit(bg_img2, [(x+4800), 0])
-        ##screen.blit(bird_img, [300,200])#  #screen Surfaiceに鳥のイメージ
+        ##screen.blit(bird_img, [300-tmr,200])#  #screen Surfaiceに鳥のイメージ
         screen.blit(bird_img, bird_rct)
+        dx, dy = 0, 0  
         if key_lst[pg.K_UP]:
-            bird_rct.move_ip((0, -1))
-        elif key_lst[pg.K_DOWN]:
-            bird_rct.move_ip((0, +1))
-        elif key_lst[pg.K_RIGHT]:
-            bird_rct.move_ip((+1, 0))
-        elif key_lst[pg.K_LEFT]:
-            bird_rct.move_ip((-1, 0))
+            dy = -1
+        if key_lst[pg.K_DOWN]:
+            dy = 1
+        if key_lst[pg.K_RIGHT]:
+            dx = 1
+        else :
+            dx = -1
+        if key_lst[pg.K_LEFT]:
+            dx = -1
+
+        bird_rct.move_ip(dx, dy)
+
+        
        
         pg.display.update()
         tmr += 1        
-        clock.tick(500)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
