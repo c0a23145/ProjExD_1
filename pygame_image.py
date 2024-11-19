856608a8727334c65d12498365a6d438edd0cb65
Ayato <c0a2314523@edu.teu.ac.jp>
2024-11-19 16:13:49 +0900

    練習7:ループ解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,25 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     bird_img = pg.image.load("fig/3.png")
     bird_img = pg.transform.flip(bird_img, True, False)##鳥の左右反転
     tmr = 0
     while True:
+        
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        x = -(tmr%3200)
+        screen.blit(bg_img, [(x), 0])
+        screen.blit(bg_img2, [(x+1600), 0])
+        screen.blit(bg_img, [(x+3200), 0])
+        screen.blit(bg_img2, [(x+4800), 0])
         screen.blit(bird_img, [300,200]) #screen Surfaiceに鳥のイメージ
+    
+       
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
